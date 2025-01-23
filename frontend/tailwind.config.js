a0ada221c2ab778a6b0e9f750408714b67d12ca3
JDM_David <fer.mtz.guz@gmail.com>
2025-01-23 14:44:26 -0600

    .

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Agrega las rutas de tus archivos
+    "./css/**/*.css",
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
