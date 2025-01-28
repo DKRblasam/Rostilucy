9d2c72bce70dc5ff7fac0692014099cf64f3fc2c
JDM_David <fer.mtz.guz@gmail.com>
2025-01-27 23:00:50 -0600

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 
 module.exports = {
   content: [
-    "../index.html",
-    "../pruebas.html",
-    "../frontend/*.html",
-    "../frontend/**/*.html",
-    "../frontend/css/**/*.css",
+    "./index.html",
+    "./pruebas.html",
+    "./frontend/*.html",
+    "./frontend/**/*.html",
+    "./frontend/css/**/*.css",
   ],
   theme: {
     extend: {},
